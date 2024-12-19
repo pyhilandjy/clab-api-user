9389dce04bd7c32eaccf8de8fdad96d561b2096b
pyhilandjy <hilandjy@gmail.com>
2024-12-19 15:01:06 +0900

    refactor:Path Parameter 추가

--- app/routers/reports.py
+++ app/routers/reports.py
@@ -15,31 +15,31 @@ from app.services.reports import (
 router = APIRouter()
 
 
-@router.get("/cover/data", tags=["User_Report"])
+@router.get("/cover/data/{user_reports_id}", tags=["User_Report"])
 def get_cover_data(user_reports_id):
     return select_cover_data(user_reports_id)
 
 
-@router.get("/wordcloud/data", tags=["User_Report"])
+@router.get("/wordcloud/data/{user_reports_id}", tags=["User_Report"])
 def get_wordcloud_data(user_reports_id):
     return select_wordcloud_data(user_reports_id)
 
 
-@router.get("/sentence_length/data", tags=["User_Report"])
+@router.get("/sentence_length/data/{user_reports_id}", tags=["User_Report"])
 def get_sentence_length_data(user_reports_id):
     return select_sentence_length_data(user_reports_id)
 
 
-@router.get("/pos_ratio/data", tags=["User_Report"])
+@router.get("/pos_ratio/data/{user_reports_id}", tags=["User_Report"])
 def get_pos_ratio_data(user_reports_id):
     return select_pos_ratio_data(user_reports_id)
 
 
-@router.get("/speech_act/data", tags=["User_Report"])
+@router.get("/speech_act/data/{user_reports_id}", tags=["User_Report"])
 def get_speech_act_data(user_reports_id):
     return select_speech_act_data(user_reports_id)
 
 
-@router.get("/insight/data", tags=["User_Report"])
+@router.get("/insight/data/{user_reports_id}", tags=["User_Report"])
 def get_insight_data(user_reports_id):
     return select_insight_data(user_reports_id)
