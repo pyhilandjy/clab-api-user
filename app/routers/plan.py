650aa0ee974fd8fb132c5dac947d5429861afe7d
pyhilandjy <hilandjy@gmail.com>
2025-01-06 11:53:49 +0900

    refactor: 안쓰는 앤드포인트 주석

--- app/routers/plan.py
+++ app/routers/plan.py
@@ -22,50 +22,50 @@ async def get_plans():
     return plan
 
 
-@router.post("/user/plans/{plan_id}", tags=["Plan"])
-async def post_user_plan(plan_id: str, current_user=Depends(get_current_user)):
-    """user_plans, user_missions, user_reports insert"""
-    try:
-        user_id = current_user.get("sub")
-        if not user_id:
-            raise HTTPException(status_code=400, detail="Invalid user ID")
-        update_user_plan_mission(user_id, plan_id)
-        return {"message": "User plan updated successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @router.post("/user/plans/{plan_id}", tags=["Plan"])
+# async def post_user_plan(plan_id: str, current_user=Depends(get_current_user)):
+#     """user_plans, user_missions, user_reports insert"""
+#     try:
+#         user_id = current_user.get("sub")
+#         if not user_id:
+#             raise HTTPException(status_code=400, detail="Invalid user ID")
+#         update_user_plan_mission(user_id, plan_id)
+#         return {"message": "User plan updated successfully"}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/user/plans", tags=["Plan"])
-async def post_user_plan(current_user=Depends(get_current_user)):
-    try:
-        user_id = current_user.get("sub")
-        if not user_id:
-            raise HTTPException(status_code=400, detail="Invalid user ID")
-        user_plan = select_plans_user(user_id)
-        return user_plan
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @router.get("/user/plans", tags=["Plan"])
+# async def post_user_plan(current_user=Depends(get_current_user)):
+#     try:
+#         user_id = current_user.get("sub")
+#         if not user_id:
+#             raise HTTPException(status_code=400, detail="Invalid user ID")
+#         user_plan = select_plans_user(user_id)
+#         return user_plan
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/user/missions", tags=["Plan"])
-async def post_user_plan(current_user=Depends(get_current_user)):
-    try:
-        user_id = current_user.get("sub")
-        if not user_id:
-            raise HTTPException(status_code=400, detail="Invalid user ID")
-        user_mission = select_user_missions(user_id)
-        return user_mission
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @router.get("/user/missions", tags=["Plan"])
+# async def post_user_plan(current_user=Depends(get_current_user)):
+#     try:
+#         user_id = current_user.get("sub")
+#         if not user_id:
+#             raise HTTPException(status_code=400, detail="Invalid user ID")
+#         user_mission = select_user_missions(user_id)
+#         return user_mission
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/user/missions/{user_missions_id}", tags=["Plan"])
-async def post_user_plan(user_missions_id: str, current_user=Depends(get_current_user)):
-    try:
-        user_id = current_user.get("sub")
-        if not user_id:
-            raise HTTPException(status_code=400, detail="Invalid user ID")
-        user_mission = select_user_missions_detail(user_id, user_missions_id)
-        return user_mission
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @router.get("/user/missions/{user_missions_id}", tags=["Plan"])
+# async def post_user_plan(user_missions_id: str, current_user=Depends(get_current_user)):
+#     try:
+#         user_id = current_user.get("sub")
+#         if not user_id:
+#             raise HTTPException(status_code=400, detail="Invalid user ID")
+#         user_mission = select_user_missions_detail(user_id, user_missions_id)
+#         return user_mission
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
